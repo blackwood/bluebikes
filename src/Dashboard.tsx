d8cd22a970d25d375287e6bc33fe85f36c9f3ae2
Miles Blackwood <miles@blackwood.io>
2024-06-13 23:53:33 -0400

    Correct zoom, include muni filter.

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,29 +1,63 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Navigation from "./Navigation";
+import stations from "./../data/stations";
+import munis from "./../data/municipalities.json";
+import DashboardHeader from "./DashboardHeader";
+import { useCallback, useState } from "react";
+import MunicipalitySelect from "./MunicipalitySelect";
+import { produce } from "immer";
 
 export default function Dashboard() {
+  const [filters, setFilters] = useState({
+    municipality: null,
+  });
+
+  const handleSelect = useCallback((value) => {
+    setFilters(
+      produce((draft) => {
+        console.log({ value, draft });
+        draft.municipality = value;
+      })
+    );
+  }, []);
+
+  console.log(filters);
+
   return (
     <>
       <Navigation />
-
       <header className="bg-white shadow">
-        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Dashboard
-          </h1>
-        </div>
+        <DashboardHeader />
       </header>
       <main className="flex-grow flex-col">
         <div className="grid grid-cols-2 h-full">
-          <MapContainer center={[42.3736, -71.1097]} zoom={14}>
+          <MapContainer center={[42.3736, -71.1097]} zoom={11}>
             <TileLayer
               url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
               attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ"
             />
+            {stations
+              .filter(
+                (station) =>
+                  !filters.municipality ||
+                  station.municipality === filters.municipality
+              )
+              .map((station) => {
+                return (
+                  <Marker
+                    key={station.id}
+                    position={[station.lat, station.long]}
+                  >
+                    <Popup>{station.name}</Popup>
+                  </Marker>
+                );
+              })}
           </MapContainer>
           <div className="bg-transparent">
-            <div></div>
+            <div>
+              <MunicipalitySelect munis={munis} onSelect={handleSelect} />
+            </div>
           </div>
         </div>
       </main>
---
+++ src/MunicipalitySelect.tsx
@@ -0,0 +1,27 @@
+export default ({ munis, onSelect }) => (
+  <form class="max-w-sm mx-auto">
+    <label
+      for="countries"
+      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    >
+      Select an option
+    </label>
+    <select
+      onChange={({ target: { value } }) => onSelect(value)}
+      id="countries"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    >
+      <option selected value="">
+        Choose a municipality
+      </option>
+
+      {munis.map((muni) => {
+        return (
+          <option key={muni} value={muni}>
+            {muni}
+          </option>
+        );
+      })}
+    </select>
+  </form>
+);
